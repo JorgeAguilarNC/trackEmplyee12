35331019d35a6d8de6ee49ad4238641b58dd66c4
JorgeAguilarNC <jorge.aguilarnc@gmail.com>
2022-10-24 22:16:33 -0400

    added roles and dept

--- utilities/queries.js
+++ utilities/queries.js
@@ -15,4 +15,95 @@ const validateString = (input) => {
         return true;
     };
 };
--A
+
+// view departments
+async function showDepartments() {
+    // Connect to database
+    const db = await mysql.createConnection(
+        {
+            'host': 'localhost',
+            'user': 'root',
+            'database': 'company'
+        },
+        console.log("Database connection established. Using database 'company'.")
+    );
+
+    const sql = `SELECT
+                name AS Department,
+                id AS ID
+                FROM departments
+                ORDER BY ID ASC`;
+
+    const [rows, fields] = await db.execute(sql);
+    console.log(`\n`);
+    console.table(rows);
+    console.log(`\n\n\n\n\n\n\n\n\n`);
+};
+
+// view  roles
+async function showRoles() {
+    // Connect to database
+    const db = await mysql.createConnection(
+        {
+            'host': 'localhost',
+            'user': 'root',
+            'database': 'company'
+        },
+        console.log("Database connection established. Using database 'company'.")
+    );
+
+    const sql = `SELECT title AS Title,
+                roles.id AS ID,
+                departments.name AS Department,
+                salary AS Salary
+                FROM roles
+                LEFT JOIN departments
+                ON roles.department_id = departments.id
+                ORDER BY Department ASC, Salary DESC`;
+
+    const [rows, fields] = await db.execute(sql);
+    console.log(`\n`);
+    console.table(rows);
+    console.log(`\n\n\n\n\n\n\n\n\n`);
+};
+
+// view  employees
+async function showEmployees() {
+    // Connect to database
+    const db = await mysql.createConnection(
+        {
+            'host': 'localhost',
+            'user': 'root',
+            'database': 'company'
+        },
+        console.log("Database connection established. Using database 'company'.")
+    );
+
+    const sql = `SELECT e.id AS ID,
+                e.first_name AS First_Name,
+                e.last_name AS Last_Name,
+                roles.title AS Title,
+                departments.name AS Department,
+                roles.salary AS Salary,
+                COALESCE(CONCAT(m.first_name, ' ', m.last_name),'None') AS Manager
+                FROM employees e
+                LEFT JOIN roles
+                ON e.role_id = roles.id
+                LEFT JOIN departments
+                ON e.department_id = departments.id
+                LEFT JOIN employees m
+                ON e.manager_id = m.id
+                ORDER BY Department ASC, Salary DESC`;
+
+    const [rows, fields] = await db.execute(sql);
+    console.log(`\n`);
+    console.table(rows);
+    console.log(`\n\n\n\n\n\n\n\n`);
+};
+
+module.exports = {
+    validateString,
+    showDepartments,
+    showRoles,
+    showEmployees
+};
